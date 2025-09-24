5ee5fd0749667d3fe8ae7b8aa7f25a58568ed7c9
washwheelsww-creator <washwheels.ww@gmail.com>
2025-09-24 16:47:17 -0500

    agregqn HELPsabe q

--- app.config.js
+++ app.config.js
@@ -1,55 +1,68 @@
+// app.config.js
 import 'dotenv/config';
-export default ({config}) => ({
-  expo: {
-    name: "Wash Wheels",
-    slug: "wash-wheels-app",
-    platforms: ["ios", "android"],
-    scheme: "washwheels",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    splash: {
-      image: "./assets/images/splash-icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-      jsEngine: "jsc",
-      bundleIdentifier: "com.anonymous.washwheelsapp",
-      supportsTablet: true
-    },
-    android: {
-      jsEngine: "jsc",
-      package: "com.anonymous.washwheelsapp",
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#FFFFFF"
-      }
-    },
-    web: {
-      favicon: "./assets/images/favicon.png"
-    },
-    extra: {
-      apiKey: process.env.EXPO_FIREBASE_API_KEY,
-      authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.EXPO_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.EXPO_FIREBASE_APP_ID,
-      measurementId: process.env.EXPO_FIREBASE_MEASUREMENT_ID,
 
-      eas: {
-        projectId: "fd3356da-e1be-4634-aba2-a8b1a9dd71ba"
-      }
+export default ({ config }) => {
+  // Cuando EXPO_USE_DEV_CLIENT===true cargamos plugins nativos
+  const useDevClient = process.env.EXPO_USE_DEV_CLIENT === 'true';
 
-    },
-    jsEngine: "jsc",
-    plugins: [                  
-      [ "@rnmapbox/maps",
-        { "RNMapboxAccessToken": "pk.eyJ1IjoiZXppbmR1c3RyaWFsIiwiYSI6ImNtYWhiOHppMjAzbHIya3ExZ2kxbnZ3YTMifQ.TrxOg6CY4SF1b34CgydOYg" }]]
+  return {
+    ...config,
+    expo: {
+      ... config,
+      name: "Wash Wheels",
+      slug: "wash-wheels-app",
+      platforms: ["ios", "android"],
+      scheme: "washwheels",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/images/icon.png",
+      splash: {
+        image: "./assets/images/splash-icon.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      },
+      updates: {
+        fallbackToCacheTimeout: 0
+      },
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        jsEngine: "jsc",
+        bundleIdentifier: "com.anonymous.washwheelsapp",
+        supportsTablet: true
+      },
+      android: {
+        jsEngine: "jsc",
+        package: "com.anonymous.washwheelsapp",
+        adaptiveIcon: {
+          foregroundImage: "./assets/images/adaptive-icon.png",
+          backgroundColor: "#FFFFFF"
+        }
+      },
+      web: {
+        favicon: "./assets/images/favicon.png"
+      },
+      extra: {
+        apiKey:           process.env.EXPO_FIREBASE_API_KEY,
+        authDomain:       process.env.EXPO_FIREBASE_AUTH_DOMAIN,
+        projectId:        process.env.EXPO_FIREBASE_PROJECT_ID,
+        storageBucket:    process.env.EXPO_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId:process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
+        appId:            process.env.EXPO_FIREBASE_APP_ID,
+        measurementId:    process.env.EXPO_FIREBASE_MEASUREMENT_ID,
+        eas: {
+          projectId: "fd3356da-e1be-4634-aba2-a8b1a9dd71ba"
+        }
+      },
+      // Sólo carga plugins nativos en Dev Client
+      plugins: [
+        useDevClient && 'expo-dev-client',
+        useDevClient && [
+          '@rnmapbox/maps',
+          {
+            RNMapboxAccessToken: process.env.MAPBOX_TOKEN
+          }
+        ]
+      ].filter(Boolean)
     }
-});
+  };
+};
--- components/MapViewBox.js
+++ components/MapViewBox.js
@@ -1,17 +1,28 @@
 // components/MapViewBox.js
-import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
-import useGlobalStyles from '../styles/global';
+import Constants from 'expo-constants'
+import MapView, { UrlTile } from 'react-native-maps'
 
-export function MapViewBox() {
- const styles = useGlobalStyles();
+export default function MapboxTileMap() {
+  const token = Constants.expoConfig.extra.MAPBOX_TOKEN
+  const urlTemplate = 
+    `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}` +
+    `?access_token=${token}`
+
   return (
-    <View style={styles.container}>
-      <WebView
-        style={styles.web}
-        source={require('../assets/map.html')}
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 19.0,
+        longitude: -98.2,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05
+      }}
+    >
+      <UrlTile
+        urlTemplate={urlTemplate}
+        maximumZ={18}
+        flipY={false}
       />
-    </View>
+    </MapView>
   )
 }
-
